2338bcdcee5de00d3d53551e29605b8ba9b40525
martyryan947@gmail.com <martyryan947@gmail.com>
2026-06-20 12:49:15 +0000

    REFINED MODEL MICROS, EXCLUDING DBT COLUMNS IN SNAPSHOT AND RENAMING DBT_UPDATED_TO

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,15 +1,13 @@
 WITH
 current_from_snapshot as (
-    select * EXCLUDE (DBT_SCD_ID, DBT_UPDATED_AT,
-                           DBT_VALID_FROM, DBT_VALID_TO)
-    from {{ref('SNSH_ABC_BANK_POSITION')}}
-    where DBT_VALID_TO is null
+    {{ current_from_snapshot(
+        snsh_ref = ref('SNSH_ABC_BANK_POSITION')
+    )}}
 )
-
 select
     *,
     POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT,
-    ROUND(UNREALIZED_PROFIT / COST_BASE, 5) AS UNREALIZED_PROFIT_PCT
+    ROUND(UNREALIZED_PROFIT / COST_BASE, 5) * 100 AS UNREALIZED_PROFIT_PCT
 from current_from_snapshot
 
 
